8776251abe62943c57c89778fab37dd49a4f1b44
NotFenir <mwisnia24@gmail.com>
2026-02-23 23:44:15 +0100

    feat(ui): add tailwind css v4 standalone

--- AgriLog/AgriLog/settings.py
+++ AgriLog/AgriLog/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     "crops.apps.CropsConfig",
+    "tailwind",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
