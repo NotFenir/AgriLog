91605482fd6e455e68900f610c6825ee351f263d
NotFenir <mwisnia24@gmail.com>
2026-02-23 15:22:09 +0100

    Add crops app to AgriLog

--- AgriLog/AgriLog/settings.py
+++ AgriLog/AgriLog/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    "crops.apps.CropsConfig",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
