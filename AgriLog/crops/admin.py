9167bc2f72711ff651a61e178c1dbaaef014e3b8
NotFenir <mwisnia24@gmail.com>
2026-02-23 16:20:46 +0100

    feat(admin): register Field, CropType and Cultivation models

--- AgriLog/crops/admin.py
+++ AgriLog/crops/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Field, CropType, Cultivation
+
+
+@admin.register(CropType)
+class CropTypeAdmin(admin.ModelAdmin):
+    list_display = ["name", "created", "updated"]
+    list_display_links = ["name"]
+    search_fields = ["name"]
+
+
+@admin.register(Field)
+class FieldAdmin(admin.ModelAdmin):
+    list_display = ["name", "area_size", "owner", "created", "updated"]
+    list_display_links = ["name"]
+    list_filter = ["owner"]
+    search_fields = ["name"]
+
+
+@admin.register(Cultivation)
+class CultivationAdmin(admin.ModelAdmin):
+    list_display = ["field", "crop_type", "status", "year", "created", "updated"]
+    list_filter = ["status", "year", "field", "crop_type"]
