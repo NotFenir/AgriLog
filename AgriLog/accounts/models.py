ddb42f5a5cfd0458d2a65d5ddfabf76ab7f376ae
NotFenir <mwisnia24@gmail.com>
2026-02-24 21:26:14 +0100

    Add profile model

--- AgriLog/accounts/models.py
+++ AgriLog/accounts/models.py
@@ -1,3 +1,12 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    @property
+    def total_area(self):
+        return (
+            self.user.fields.aggregate(models.Sum("area_size"))["area_size__sum"] or 0
+        )
